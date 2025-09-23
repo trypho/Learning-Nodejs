a830217b005a53b4a272059f5717e890f4e990d1
trypho <kgobemadimetja257@gmail.com>
2025-09-24 01:59:30 +0200

    Added the route file to handle routing

--- app.js
+++ app.js
@@ -1,45 +1,8 @@
 const http = require('http');
-const fs = require('fs');
 
-const server = http.createServer((req, res) => {
-    const url = req.url;
-    const method = req.method;
+const routes = require('./routes');
 
-    if (url === '/') {
-        res.write('<html>');
-        res.write('<head><title>Message Page</title></head>');
-        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button>Send</button></form></body>');
-        res.write('</html>');
-        return res.end();
-    }
-
-    if (url === '/message' && method === 'POST') {
-        const body = [];
-
-        req.on('data', (chunk) => {
-            body.push(chunk);
-        });
-        
-       return req.on('end', () => {
-            const parsedBody = Buffer.concat(body).toString();
-            const message = parsedBody.split('=')[1];
-            fs.writeFile('message.txt', message,() => {
-                   res.statusCode = 302;
-                    res.setHeader('Location', '/');
-                    return res.end();
-            });
-        });
-
-    }
-
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<html>');
-    res.write('<head><title>My First Page</title></head>');
-    res.write('<body><h1>Hello Mr KGOBE from my Node.js server!</h1></body>');
-    res.write('</html>');
-    res.end();
-
-});
+const server = http.createServer(routes);
 
 server.listen(3000);
 console.log('Server is listening on port 3000');

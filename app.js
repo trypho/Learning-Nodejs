79f46154b2ac548c38961ffaac5f4794614420b2
trypho <kgobemadimetja257@gmail.com>
2025-10-04 18:24:19 +0200

    added the file not found handler code

--- app.js
+++ app.js
@@ -10,5 +10,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(adminRoute);
 app.use(shopRoute);
 
+app.use((req, res, next) => {
+  res.status(404).send("<h1>Page not found</h1>");
+});
+
 app.listen(3000);
-console.log("Server is listening on port 3000");
--- routes/shop.js
+++ routes/shop.js
@@ -3,7 +3,6 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
-  console.log("In the DEFAULT middleware");
   res.send("<h1>Hello From Express!!</h1>");
 });
 

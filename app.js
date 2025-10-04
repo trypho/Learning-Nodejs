2b096316b00698aef4d057754bb2eca40f3d11fb
trypho <kgobemadimetja257@gmail.com>
2025-10-04 16:15:02 +0200

    routing in express.js

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require("express");
 
 const app = express();
-app.use((req, res, next) => {
+
+app.use("/secondPage", (req, res, next) => {
   console.log("In the middleware");
-  next();
+  res.send("<h1>Second Page loaded</h1>");
 });
-app.use((req, res, next) => {
+
+app.use("/", (req, res, next) => {
   console.log("In another middleware");
   res.send("<h1>Hello From Express!!</h1>");
 });
